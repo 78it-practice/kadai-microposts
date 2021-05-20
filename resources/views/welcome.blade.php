aa900f0aaf375535b5dcf8bc5d951b9f829ec714
78it.practice <78it.ex.jp>
2021-05-20 09:51:27 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 @section('content')
-  <div clsas="center jumbotron">
-    <div class="text-center">
-      <h1>Welcome to the Microposts</h1>
-      {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+  @if(Auth::check())
+    <h2>{{ Auth::user()->name }}</h2>
+  @else
+    <div clsas="center jumbotron">
+      <div class="text-center">
+        <h1>Welcome to the Microposts</h1>
+        {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+      </div>
     </div>
-  </div>
+  @endif
 @endsection
